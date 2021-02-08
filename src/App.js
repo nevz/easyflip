739921b7f5f6e77ca22e1f37b4294cdb13ba5a2a
nevz <sreisenegger@gmail.com>
2021-02-08 14:31:19 -0300

    added user token to localstorage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { JitsiRoom } from './JitsiRoom';
 import { JoinRoom } from './JoinRoom';
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,6 +11,13 @@ import {
 
 
 function App() {
+  useEffect(() => {
+    const userToken = localStorage.getItem('userToken');
+    if(!userToken){
+      const newToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      localStorage.setItem('userToken', newToken);
+    }
+  }, [])
 
   return (
     <div className="App">
